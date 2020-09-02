efdb2bbb86a6a6c70a0384180f22a3a24676d71c
Boul3ez85 <https://github.com/Boul3ez85>
2020-09-02 20:21:57 +0100

    update code

---
+++ models/state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" State Module for HBNB project."""
+from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+import os
+import models
+from models import storage
+from models.city import City
+
+
+class State(BaseModel, Base):
+    """ State class """
+
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City",  backref="state", cascade="all,delete")
+    if os.getenv("HBNB_TYPE_STORAGE") != 'db':
+        @property
+        def cities(self):
+            liste = []
+            for city in list(storage.all(City).values()):
+                if city.state_id == self.id:
+                    liste.append(city)
+            return liste
---
+++ models/user.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""This module defines a class User"""
+from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+
+
+class User(BaseModel, Base):
+    """Represents a user for MySQL db
+    Attributes:
+        __tablename__ (str): the name of MySQL table.
+        email (SQLAlchemy string): the email of the user.
+        password (SQLAlchemy string): the password of the user.
+        first_name (SQLAlchemy string): the user's first name
+        last_name (SQLAlchemy string): the user's last name
+    """
+
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place", backref="user", cascade="delete")
+    reviews = relationship("Review", backref="user", cascade="delete")
