a8825d3f7fb5c0d324fbc16fae2019dfe26bc189
Boul3ez85 <benrebah.moez@gmail.com>
2020-09-02 09:24:11 +0100

    fix base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
-import uuid
+from uuid import uuid4
 import models
 from sqlalchemy import DateTime
 from datetime import datetime
@@ -28,18 +28,14 @@ class BaseModel:
             *args (any argument): unused.
             **kwargs (dictionary): key/value pairs of attributes.
         """
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-        for k, v in kwargs.items():
-            setattr(self, k, v)
+        self.id = str(uuid4())
+        self.created_at = self.updated_at = datetime.utcnow()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
 
     def __str__(self):
         """Returns a string representation of the instance"""
